cbd1939c2e561e10ffa7085be22a4d590be336c1
adis98 <aditya.ssr@gmail.com>
2024-10-24 19:42:38 +0200

    Strided hyacinth pipeline initial commit

--- ablation_parallelism.sh
+++ ablation_parallelism.sh
@@ -9,13 +9,17 @@ for dataset in "${options_dataset[@]}"
 do
   for synth_mask in "${options_synth_mask[@]}"
   do
-    python3.12 synthesis_hyacinth_pipeline.py -d $dataset -synth_mask $synth_mask
+    python3.12 synthesis_hyacinth_pipeline_strided.py -d $dataset -synth_mask $synth_mask -stride 1
+    python3.12 synthesis_hyacinth_pipeline_strided.py -d $dataset -synth_mask $synth_mask -stride 8
+    python3.12 synthesis_hyacinth_pipeline_strided.py -d $dataset -synth_mask $synth_mask -stride 16
+    python3.12 synthesis_hyacinth_pipeline_strided.py -d $dataset -synth_mask $synth_mask -stride 32
+    #python3.12 synthesis_hyacinth_pipeline.py -d $dataset -synth_mask $synth_mask
 
-    python3.12 synthesis_hyacinth_divide_and_conquer.py -d $dataset -synth_mask $synth_mask
-    python3.12 synthesis_hyacinth_autoregressive.py -d $dataset -synth_mask $synth_mask -stride 8
+    #python3.12 synthesis_hyacinth_divide_and_conquer.py -d $dataset -synth_mask $synth_mask
+    #python3.12 synthesis_hyacinth_autoregressive.py -d $dataset -synth_mask $synth_mask -stride 8
 
-    python3.12 synthesis_hyacinth_autoregressive.py -d $dataset -synth_mask $synth_mask -stride 16
+    #python3.12 synthesis_hyacinth_autoregressive.py -d $dataset -synth_mask $synth_mask -stride 16
 
-    python3.12 synthesis_hyacinth_autoregressive.py -d $dataset -synth_mask $synth_mask -stride 32
+    #python3.12 synthesis_hyacinth_autoregressive.py -d $dataset -synth_mask $synth_mask -stride 32
   done
 done
